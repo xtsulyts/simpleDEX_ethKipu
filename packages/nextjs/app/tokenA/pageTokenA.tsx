897f732f7fd89484c1bcfbd0d72c4f5e7436493e
xtsulyts <531xtsulyts@gmail.com>
2024-12-15 23:27:03 -0300

    Creo pageTokenB, pegeSimpleDex con sus interfaces

--- packages/nextjs/app/tokenA/pageTokenA.tsx
+++ packages/nextjs/app/tokenA/pageTokenA.tsx
@@ -33,18 +33,16 @@ const TokenA: NextPage = () => {
     } catch (e) {
       console.error("Error setting greeting:", e);
     }
-  }
-
-
+  };
   return (
     <>
       <div className="flex items-center flex-col text-center mt-8  p-10">
         <h1 className="text-4xl my-0">Token A</h1>
         <div className="card bg-base-100 w-96 shadow-xl">
           <div className="card-body">
-            <h2 className="card-title flex-col text-center">Token A</h2>
+            <h2 className="card-title flex-col text-center">Transacction</h2>
             <label>Actual Ammount</label>
-            <InputBase name="ActuAlamount" disabled placeholder="Actual Amount" value={formatEther(tokenBalance || BigInt(0))} onChange={setUrl} />
+            <InputBase name="actuAlamount" disabled placeholder="Actual Amount" value={formatEther(tokenBalance || BigInt(0))} onChange={setUrl} />
             <label>Amount to Mint</label>
             <IntegerInput
               value={amountMint}
@@ -53,7 +51,7 @@ const TokenA: NextPage = () => {
               }}
               placeholder="value (wei)"
             />
-            <label>Approve</label>
+            <label>To address</label>
             <AddressInput onChange={setAddress} value={address} placeholder="Input your address" />
             <div className="card-actions justify-end">
               <button className="btn btn-primary w-full mt-2" onClick={() => { handleMint }}><CurrencyDollarIcon className="h-4 w-4" />Approve Now</button>
--- packages/nextjs/app/tokenB/page.tsx
+++ packages/nextjs/app/tokenB/page.tsx
@@ -1,19 +1,68 @@
+"use client";
 import type { NextPage } from "next";
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+import { AddressInput } from "~~/components/scaffold-eth";
+import { IntegerInput } from "~~/components/scaffold-eth";
+import { InputBase } from "~~/components/scaffold-eth";
+import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
+import { useAccount } from 'wagmi'
+import { formatEther } from "viem";
 
-export const metadata = getMetadata({
-  title: "Token B",
-  description: "Debug your deployed 🏗 Scaffold-ETH 2 contracts in an easy way",
-});
+const TokenA: NextPage = () => {
+  const [address, setAddress] = useState("");
+  const [amountMint, setAmountMint] = useState<string | bigint>("");
+  const [atualAmount, setUrl] = useState<string>();
+  const account = useAccount();
 
-const TokenB: NextPage = () => {
+  const { data: tokenBalance } = useScaffoldReadContract({
+    contractName: "TokenB",
+    functionName: "balanceOf",
+    args: [account?.address ?? ""],
+  });
+
+  const { writeContractAsync: mintTokens } = useScaffoldWriteContract("TokenB");
+
+  const handleMint = async () => {
+    try {
+      await mintTokens({
+        functionName: "approve",
+        args: [address, BigInt(amountMint)],
+      });
+    } catch (e) {
+      console.error("Error setting greeting:", e);
+    }
+  }
+
+
   return (
     <>
-      <div className="text-center mt-8 bg-secondary p-10">
+      <div className="flex items-center flex-col text-center mt-8  p-10">
         <h1 className="text-4xl my-0">Token B</h1>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title flex-col text-center">Transacction</h2>
+            <label>Actual Ammount</label>
+            <InputBase name="actuAlamount" disabled placeholder="Actual Amount" value={formatEther(tokenBalance || BigInt(0))} onChange={setUrl} />
+            <label>Amount to Mint</label>
+            <IntegerInput
+              value={amountMint}
+              onChange={updatedAmount => {
+                setAmountMint(updatedAmount);
+              }}
+              placeholder="value (wei)"
+            />
+            <label>To address</label>
+            <AddressInput onChange={setAddress} value={address} placeholder="Input your address" />
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary w-full mt-2" onClick={() => { handleMint }}><CurrencyDollarIcon className="h-4 w-4" />Approve Now</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
-export default TokenB;
+export default TokenA;
